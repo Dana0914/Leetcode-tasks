9e4afa4567dfee06bfccc7dc3ff3c5e036582fce
Dana <Dana.ackerman0914@gmail.com>
2024-07-30 22:08:57 +0500

    Valid Anagram

---
+++ src/leetcodetasks/ValidAnagram.java
@@ -0,0 +1,23 @@
+package leetcodetasks;
+
+import java.util.Arrays;
+
+public class ValidAnagram {
+    public static void main(String[] args) {
+        String s = "anagram";
+        String t = "nagaram";
+        System.out.println(isAnagram(s, t));
+
+
+    }
+    public static boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        char[] chars1 = s.toCharArray();
+        char[] chars2 = t.toCharArray();
+        Arrays.sort(chars1);
+        Arrays.sort(chars2);
+        return Arrays.toString(chars1).equals(Arrays.toString(chars2));
+    }
+}
